76fe035e89e4835ea347253c2ded673e4e65694d
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-09-29 21:29:07 -0700

    [question 3] added q3

---
+++ QUESTION 3/go.mod
@@ -0,0 +1,5 @@
+module main.go
+
+go 1.15
+
+require github.com/snksoft/crc v1.1.0
---
+++ QUESTION 3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"hash/crc32"
+	"log"
+	"sync"
+	"time"
+	"math/rand"
+)
+var semap sync.Mutex
+var MAX_WORKER =100
+func main() {
+	start := time.Now()
+	semap.Lock()
+	elapsed := time.Since(start)
+
+	for i:=0;i<MAX_WORKER;i++{
+		go crack(i)
+	}
+
+	semap.Lock()
+	log.Printf("Procress took %s", elapsed)
+}
+
+func crack(num int){
+	fmt.Println("worker # ",num," online ")
+	data := "102E91D22C3795494B378096783BE2A3"
+	crc32_val:=crc32.ChecksumIEEE([]byte(data))
+
+	token := make([]byte, 32)
+	rand.Read(token)
+	token_crc:=crc32.ChecksumIEEE([]byte(token))
+	var itr=0
+	for token_crc!=crc32_val{
+		itr+=1;
+		if(itr%1000000==0){
+			fmt.Println("finished #",itr)
+		}
+		rand.Read(token)
+		token_crc=crc32.ChecksumIEEE([]byte(token))
+	}
+	fmt.Println(num,": CRC token is",token_crc," and ",crc32_val)
+	semap.Unlock()
+}
