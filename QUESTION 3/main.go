41ad3bb457af75b274cc68ec553230c51eb1ea02
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-09-29 22:49:22 -0700

    pre submisison checks

--- QUESTION 3/main.go
+++ QUESTION 3/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"math/rand"
 	"log"
+	"os"
+	"strconv"
 	"sync"
 	"time"
-	"math/rand"
 )
 var semap sync.Mutex
-var MAX_WORKER =8
+var MAX_WORKER =1
 func main() {
 	start := time.Now()
 	semap.Lock()
@@ -29,18 +31,28 @@ func crack(num int){
 	data := "102E91D22C3795494B378096783BE2A3"
 	crc32_val:=crc32.ChecksumIEEE([]byte(data))
 
-	token := make([]byte, 32000000)
+	var token=make([]byte,80000)
 	rand.Read(token)
 	token_crc:=crc32.ChecksumIEEE([]byte(token))
 	var itr=0
 	for token_crc!=crc32_val{
 		itr+=1;
-		if(itr%100==0){
+		if(itr%100000==0){
 			fmt.Println("finished #",itr)
 		}
 		rand.Read(token)
-		token_crc=crc32.ChecksumIEEE(token)
+		token_crc=crc32.ChecksumIEEE([]byte(token))
 	}
 	fmt.Println(num,": CRC token is",token_crc," and ",crc32_val)
+	fmt.Println(num,": CRC val is",token[:1000])
+	var file_name = "RAND_CRYPT#"
+	file_name+=strconv.Itoa(num)
+	file_name+=".log"
+	f, err := os.Create(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, _ = f.WriteString(string(token))
+	defer f.Close()
 	semap.Unlock()
 }
